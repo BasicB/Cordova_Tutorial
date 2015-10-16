dd83f906ee99146faa3a388c6a8af4d906586933
Basicb <Basicb4@gmail.com>
2015-10-16 10:38:48 -0400

    Upload1

---
+++ assets/www/cordova_plugins.js
@@ -0,0 +1,23 @@
+cordova.define('cordova/plugin_list', function(require, exports, module) {
+module.exports = [
+    {
+        "file": "plugins/cordova-plugin-whitelist/whitelist.js",
+        "id": "cordova-plugin-whitelist.whitelist",
+        "runs": true
+    },
+    {
+        "file": "plugins/nl.x-services.plugins.flashlight/www/Flashlight.js",
+        "id": "nl.x-services.plugins.flashlight.Flashlight",
+        "clobbers": [
+            "window.plugins.flashlight"
+        ]
+    }
+];
+module.exports.metadata = 
+// TOP OF METADATA
+{
+    "cordova-plugin-whitelist": "1.0.1-dev",
+    "nl.x-services.plugins.flashlight": "2.0.3"
+}
+// BOTTOM OF METADATA
+});
